38079a45fc0f4b6c29e136a68577dd0fa928aa1b
Mark Melendez <MarkMelendez2008@outlook.com>
2022-05-29 15:34:05 +0800

    新建 温度转换.py
    Signed-off-by: Mark Melendez <MarkMelendez2008@outlook.com>

---
+++ 进阶/实战/温度转换/温度转换.py
@@ -0,0 +1,20 @@
+#!/
+# _*_coding: utf-8 _*_
+# @Time :  15:24
+# @Author : Mark Melendez
+# @File : 温度转换.py
+# @desc :
+# -----------------------------begin-------------------------------
+# 导入依赖包
+import
+
+
+# 定义必要变量（函数等）
+
+# 定义mian函数
+def main():
+
+
+# 调用main函数
+if __name__ == '__main__':
+    main()
