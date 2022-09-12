37596c587af76f20a690883bb0a3522f9d4519b1
Mark Melendez <MarkMelendez2008@outlook.com>
2022-09-12 14:10:41 +0800

    随机颜色fix

--- 进阶/实战/随机颜色/随机颜色.py
+++ 进阶/实战/随机颜色/随机颜色.py
@@ -37,7 +37,7 @@ def Creat_Showwindow(color, alpha):
 
 # 随机选取颜色
 def RandomColor(alpha):
-    ColorList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    ColorList = ["0", '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     color = ""
     if alpha:
         Number = 8
