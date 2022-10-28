98b48baa86f224ecc5f9b97a9cf4cd5d5d737387
Mark Melendez <MarkMelendez2008@outlook.com>
2022-10-28 12:40:56 +0800

    拼音转换器

---
+++ 进阶/实战/汉字转拼音/拼音转换器.py
@@ -0,0 +1,21 @@
+#!/
+# _*_coding: utf-8 _*_
+# @Time :  下午 12:39
+# @Author : Mark Melendez
+# @File : 拼音转换器.py
+# @desc :
+# -----------------------------begin-------------------------------
+# 导入依赖包
+import PySimpleGUI as Gui
+import pinyin
+
+
+# 定义必要变量（函数等）
+
+# 定义mian函数
+def main():
+
+
+# 调用main函数
+if __name__ == '__main__':
+    main()
