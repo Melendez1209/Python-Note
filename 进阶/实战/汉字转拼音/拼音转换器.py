4db86af61f24e775c8c00ee634dd426556ce1dcb
Mark Melendez <MarkMelendez2008@outlook.com>
2022-10-28 13:47:56 +0800

    拼音转换器完成

--- 进阶/实战/汉字转拼音/拼音转换器.py
+++ 进阶/实战/汉字转拼音/拼音转换器.py
@@ -6,14 +6,56 @@
 # @desc :
 # -----------------------------begin-------------------------------
 # 导入依赖包
-import PySimpleGUI as Gui
-import pinyin
+import time
 
+import PySimpleGUI as Gui
+import pyperclip
+from xpinyin import Pinyin
 
 # 定义必要变量（函数等）
+p = Pinyin()
+
+MainPage = [
+    [Gui.T("欢迎使用拼音转换器~")],
+    [Gui.T("在这里输入要转换的内容："), Gui.In()],
+    [Gui.B("确定")]
+]
+
 
 # 定义mian函数
 def main():
+    # 创建窗口
+    MainWindow = Gui.Window("拼音转换器", MainPage)
+    event, value = MainWindow.read()
+    # 窗口事件处理
+    if event is None:
+        MainWindow.close()
+    elif event == "确定":
+        if value[0] == "":
+            Gui.popup("您输入了不可转换的数据类型")
+        else:
+
+            try:
+
+                result = p.get_pinyin(value[0], " ")
+                pyperclip.copy(result)
+
+                ResultPage = [
+                    [Gui.T(result)],
+                    [Gui.T("该结果已复制到您的剪贴板")]
+                ]
+                ResultWindow = Gui.Window("转换结果页", ResultPage)
+                ResultEvent, ResultValue = ResultWindow.read()
+                if ResultEvent is None:
+                    ResultWindow.close()
+                else:
+                    pass
+
+            except:
+                Gui.popup("您输入了不可转换的数据类型")
+            MainWindow.close()
+    else:
+        pass
 
 
 # 调用main函数
